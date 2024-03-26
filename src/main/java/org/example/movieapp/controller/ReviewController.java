f003b96060dcda4b5e281e3003afc8e7410ab276
VladyslavPilkevych <90135414+VladyslavPilkevych@users.noreply.github.com>
2024-03-27 00:16:55 +0100

    review page, still with error of empty []

--- src/main/java/org/example/movieapp/controller/ReviewController.java
+++ src/main/java/org/example/movieapp/controller/ReviewController.java
@@ -20,14 +20,17 @@ public class ReviewController {
 
     @GetMapping("/reviews/{movieId}")
     public String showReviews(@PathVariable("movieId") Long movieId, Model model) {
+        System.out.printf("show review ==> ");
         List<Review> reviews = reviewService.getReviewsByMovieId(movieId);
         model.addAttribute("reviews", reviews);
-        return "reviews_list";
+        System.out.println(reviewService.getReviewsByMovieId(movieId));
+        return "movie_review";
     }
 
-    @PostMapping("/reviews/add")
-    public String addReview(@ModelAttribute("review") Review review) {
-        reviewService.addReview(review);
+    @PostMapping("/reviews/{movieId}/add")
+    public String addReview(@PathVariable("movieId") Long movieId, @ModelAttribute("review") Review review) {
+        System.out.printf("add review");
+        reviewService.addReview(movieId, review);
         return "redirect:/reviews/" + review.getMovieId();
     }
 }
--- src/main/java/org/example/movieapp/service/ReviewService.java
+++ src/main/java/org/example/movieapp/service/ReviewService.java
@@ -1,18 +1,25 @@
 package org.example.movieapp.service;
 
 import org.example.movieapp.model.Review;
+import org.example.movieapp.repository.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ReviewService {
+    @Autowired
+    private ReviewRepository reviewRepository;
 
     public List<Review> getReviewsByMovieId(Long movieId) {
-        return null;
+        return reviewRepository.findByMovieId(movieId);
     }
 
-    public void addReview(Review review) {
-
+    public void addReview(Long movieId, Review review) {
+        System.out.printf("movieId ==> ");
+        System.out.println(movieId);
+        System.out.printf("review ==> ");
+        System.out.println(review);
     }
 }
